13f83b7a8b832997c54a5fdc8700893ef1696b24
vikas99144 <vk40010@gmail.com>
2025-09-30 07:27:34 +0530

    add

--- settings/main.js
+++ settings/main.js
@@ -6,4 +6,5 @@ module.exports.configure =  async(server) => {
      await server.route(require('../modules/admin/router'));
      await server.route(require('../modules/menu/router'));
      await server.route(require('../modules/role/router'));
+     await server.route(require('../modules/setting/router'));
 }
