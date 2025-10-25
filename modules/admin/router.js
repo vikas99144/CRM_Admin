7ebb7d4e14b87a6d806c9f37c352b7bcba2b95bf
vikas99144 <vk40010@gmail.com>
2025-10-25 21:12:30 +0530

    add

--- modules/admin/router.js
+++ modules/admin/router.js
@@ -6,43 +6,43 @@ const specs = require('./specs');
 module.exports = [
     {
         method: 'POST',
-        path: '/api/v1/admin/create',
+        path: '/create',
         options: specs.create,
         handler: handler.create
     },
     {
         method: 'POST',
-        path: '/api/v1/admin/login',
+        path: '/login',
         options: specs.login,
         handler: handler.login
     },
     {
         method: 'GET',
-        path: '/api/v1/admin/view/{admin_id}',
+        path: '/view/{admin_id}',
         options: specs.view,
         handler: handler.view
     },
     {
         method: 'GET',
-        path: '/api/v1/admin/list',
+        path: '/list',
         options: specs.list,
         handler: handler.list
     },
     {
         method: 'PATCH',
-        path: '/api/v1/admin/status/{admin_id}',
+        path: '/status/{admin_id}',
         options: specs.status,
         handler: handler.status
     },
     {
         method: 'PATCH',
-        path: '/api/v1/admin/update/{admin_id}',
+        path: '/update/{admin_id}',
         options: specs.update,
         handler: handler.update
     },
     {
         method: 'DELETE',
-        path: '/api/v1/admin/delete/{admin_id}',
+        path: '/delete/{admin_id}',
         options: specs.remove,
         handler: handler.remove
     }
--- settings/main.js
+++ settings/main.js
@@ -1,10 +1,18 @@
 
 
-'use strict'
+'use strict';
 
-module.exports.configure =  async(server) => {
-     await server.route(require('../modules/admin/router'));
-     await server.route(require('../modules/acl/router'));
-     await server.route(require('../modules/role/router'));
-     await server.route(require('../modules/setting/router'));
-}
+module.exports.configure = async (server) => {
+     const prefix = '/api/v1/admin';
+     const addPrefix = (routes, prefix) => {
+          return routes.map(route => ({
+               ...route,
+               path: prefix + route.path
+          }));
+     };
+
+     await server.route(addPrefix(require('../modules/admin/router'), prefix));
+     await server.route(addPrefix(require('../modules/acl/router'), prefix));
+     await server.route(addPrefix(require('../modules/role/router'), prefix));
+     await server.route(addPrefix(require('../modules/setting/router'), prefix));
+};
